1d1b510776df6699d3f8e07144210c1e36eec1bb
ljk4268 <ljk4268@kakao.com>
2022-06-21 19:21:16 +0900

    input 1 : 사용자가 입력한 글 다루기

--- blog/src/App.js
+++ blog/src/App.js
@@ -6,9 +6,10 @@ import './App.css';
 function App() {
 
   let [title, setTitle] = useState(['남자 코트 추천','여자 코트 추천','여자 부츠 추천']);
-  let [count, setCount] = useState([0,0,0]);
+  let [count, setCount] = useState([0,0,0,0]);
   let [modal, setModal] = useState(false);
   let [order, setOrder] = useState(0)
+  let [inputValue, setInputValue] = useState('')
 
   function changeTitle(){
     let copy = [...title];
@@ -27,31 +28,16 @@ function App() {
         setTitle(copy)
       }}>클릭</button>
 
-{/* 글제목 누르면 modal창의 제목도 동일하게 나오게 기능 만들기 */}
-{/* 동적인 UI만드는 거니까 
-1. html css로 미리 디자인해놓고 
-
-2. 현재 UI의 상태를 state로 만들어두고
-
-3. state 종류에 따라서 UI가 어떻게 보일지 작성  */}
-
-{/* html css는 미리 디자인이 되어있고  
-
-현재 UI상태를 state로 만들어둔다. 
-
-let [order, setOrder] = useState(0) 이렇게.
-
-그리고 state에 따라서 UI가 어떻게 보일지 작성한다. */}
-
 
       {
         title.map(function(a, i){
           return (
               <div className="list" key={i}>
-                {/* 반복문을 돌때마다 order state가 변할 수 있게 setOrder()함수를 사용해서 order state 값을 변경해준다. */}
+                
                 <h4 onClick={()=>{ setModal(true); setOrder(i)}}>
                   {title[i]}
-                  <span onClick={()=>{ 
+                  <span onClick={(e)=>{ 
+                    e.stopPropagation();
                     let copy = [...count]
                     copy[i] ++
                     setCount(copy)
@@ -63,6 +49,21 @@ let [order, setOrder] = useState(0) 이렇게.
             )
         })
       }
+
+      <input onChange={(e)=>{ 
+      // 알아두면 좋은 리액트 동작방법
+      // state 변경함수는 늦게 처리된다.
+      // setInputValue(e.target.value) 얘가 state 변경함수고 늦게 처리되는데 이걸 비동기처리라고 한다. 
+      // setInputValue(e.target.value)가 완료되기 전에 
+      // console.log(inputValue)을 실행한다.
+        setInputValue(e.target.value) 
+        }} />
+        {/* 버튼을 누르면 사용자가 입력한 글이 리스트로 하나 추가 되는 기능만들기 */}
+        <button onClick={()=>{
+          let copy = [...title]
+          copy.push(inputValue)
+          setTitle(copy)
+        }}>추가</button>
 
       {
         modal == true ? <Modal title={title} color={'#e1f5fe'}
@@ -77,9 +78,6 @@ let [order, setOrder] = useState(0) 이렇게.
 function Modal(props){
   return(
     <div className="modal" style={{background : props.color}}>
-      {/* props로 order state를 받고 */}
-      {/* 위에서 setOrder()함수로 order state 값이 변경되면 
-      변경되는 값이 modal제목에 들어갈 수 있게 연결해준다.  */}
       <h4>{props.title[props.order]}</h4>
       <p>날짜</p>
       <p>상세내용</p>
