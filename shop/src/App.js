d2e046f39490f73cd98e886d0be89a7573cfcbd1
ljk4268 <ljk4268@kakao.com>
2022-06-23 20:46:18 +0900

    리액트 라우터 2 : navigate, nested routes, outlet

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,46 +1,40 @@
 import './App.css';
 import { Navbar, Container, Nav } from 'react-bootstrap';
 import { useState } from 'react';
+import {Routes, Route, useNavigate, Outlet} from 'react-router-dom';
+
 
 import data from './data'
-import {Routes, Route, Link, NavLink} from 'react-router-dom';
+import Detail from './routes/Detail'
 
 
 function App() {
 
   let [shoes] = useState(data)
+  // 페이지 이동을 도와주는 useNavigate()
+  let navigate = useNavigate();
 
 
   return (
     <div className="App">
 
-{/* 라우터로 페이지 나누는법  */}
-{/* 페이지를 n개 나누고 싶은지에 따라서  <Route />를 n개 사용해주면 됨.*/}
-{/* 상세페이지를 만들고 싶으면 아래 코드처럼 작성하면 됨. */}
-
-      {/* <Routes>
-        <Route path="/" element={<div>메인페이지</div>} />
-        <Route path="/detail" element={<div>상세페이지</div>} />
-      </Routes> */}
-
-
       <Navbar bg="light" variant="light">
         <Container>
           <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/detail">Cart</Nav.Link>
+            {/* 페이지 이동 도와주는 navigate */}
+            <Nav.Link onClick={()=>{ navigate('/') }}>Home</Nav.Link>
+            <Nav.Link onClick={()=>{ navigate('/detail') }}>Detail</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
 
-      <Routes>
-        {/* 메인페이지에 보여줄 html들 작성 */}
-        <Route path="/" element={<div>
-          {/* 메인페이지 이미지 */}
-          <div className="main-bg"></div>
+      
 
-          {/* 메인페이지의 상품들 */}
+      <Routes>
+        {/* 메인페이지 */}
+        <Route path="/" element={<div>
+          <div className="main-bg"></div>
             <div className="container">
               <div className="row">
 
@@ -55,8 +49,30 @@ function App() {
               </div>
             </div> 
         </div>} />
-        <Route path="/detail" element={<div>상세페이지</div>} />
+
+        {/* 디테일페이지 */}
+        <Route path="/detail" element={<Detail/>} />
+
+        {/* Nested Route */}
+        {/* /about/member 페이지도 만들고*/}
+        {/* /about/location 페이지도 만들고*/}
+        <Route path="/about" element={<About/>}>
+          {/* /about/member 페이지도 만들고*/}
+          {/* 보여주고 싶은 내용은 컴포넌트에 <Outlet></Outlet>넣어줘야함 */}
+          <Route path="member" element={<div>멤버임</div>} />
+          {/* /about/location 페이지도 만들고*/}
+          <Route path="location" element={<About/>} />
+        </Route>
+
+
+
+
+        {/* 404페이지 */}
+        {/* path="*" 지정한 라우트외에 모든 페이지에서 보여줄 페이지  */}
+        <Route path="*" element={<div>없는페이지요</div>} />
       </Routes>
+
+      
 
     
 
@@ -72,6 +88,15 @@ function ShoesList(props){
     <h4>{props.shoe.title}</h4>
     <p>{props.shoe.price}</p>
   </div>
+    )
+}
+
+function About(){
+  return(
+      <div>
+        <h4>회사정보임</h4>
+        <Outlet></Outlet>
+      </div>
     )
 }
 
