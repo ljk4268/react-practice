a3612b83b14a0ced131b459c79669d17ef86fd20
ljk4268 <ljk4268@kakao.com>
2022-06-27 15:12:06 +0900

    Redux 2 : store에 state 보관하고 쓰는 법

--- shop/src/routes/Cart.js
+++ shop/src/routes/Cart.js
@@ -1,8 +1,15 @@
 import { Table } from 'react-bootstrap'
+import { useSelector } from 'react-redux';
 
 // Reducx를 사용하면 컴포넌트들이 props 없이 state 공유가 가능하다. 
 
 function Cart(){
+
+// 리덕스 store를 가져와줌
+// 리덕스 store에 있는 모든 state가 남음
+// state.작명 쓰면 내가 원하는 state만 가져다 쓸 수 있음 
+  let stock = useSelector((state)=> state.stock )
+
   return(
     <div>
       <Table>
@@ -15,12 +22,19 @@ function Cart(){
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>안녕</td>
-            <td>안녕</td>
-            <td>안녕</td>
-          </tr>
+        {
+          stock.map(function(shoes, i){
+            return(
+              <tr key={i}>
+                <td>1</td>
+                <td>{shoes.name}</td>
+                <td>{shoes.count}</td>
+                <td>수정중</td>
+              </tr>
+            )
+          })
+        }
+          
         </tbody>
       </Table> 
     </div>
--- shop/src/store.js
+++ shop/src/store.js
@@ -1,7 +1,33 @@
 // store.js는 state들을 보관하는 파일
+// 리덕스 쓰는이유
+// 1. 컴포넌트간 state 공유가 편해짐
 
-import { configureStore } from '@reduxjs/toolkit'
+
+import { configureStore, createSlice } from '@reduxjs/toolkit'
+
+
+// createSlice는 useState 역할임
+// // 여기는 state를 만드는 공간
+let user = createSlice({
+  name : 'user',
+  initialState: 'kim'
+})
+
+let stock = createSlice({
+  name : 'stock',
+  initialState: [
+    {id : 0, name : 'White and Black', count : 2},
+    {id : 2, name : 'Grey Yordan', count : 1}
+  ] 
+})
+
+
 
 export default configureStore({
-  reducer: { }
+  // 여기는 state를 등록하는 공간
+  reducer: {
+    user : user.reducer,
+    stock : stock.reducer
+  
+  }
 })
