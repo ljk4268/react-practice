7c4f49cfb6bef636516a59e713a293645b88a037
ljk4268 <ljk4268@kakao.com>
2022-06-27 15:31:29 +0900

    Redux 3 : store의 state 변경하는 법

--- shop/src/routes/Cart.js
+++ shop/src/routes/Cart.js
@@ -1,5 +1,7 @@
 import { Table } from 'react-bootstrap'
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+// redux state 변경함수 import 해오고 
+import { changeName } from './../store'
 
 // Reducx를 사용하면 컴포넌트들이 props 없이 state 공유가 가능하다. 
 
@@ -8,10 +10,16 @@ function Cart(){
 // 리덕스 store를 가져와줌
 // 리덕스 store에 있는 모든 state가 남음
 // state.작명 쓰면 내가 원하는 state만 가져다 쓸 수 있음 
-  let stock = useSelector((state)=> state.stock )
+  let cart = useSelector((state)=> state.cart )
+  let user = useSelector((state)=> state.user )
+  // redux state 변경함수 사용할 수 있게 도와주는 useDispatch함수 변수로 만들어서 
+  let dispatch = useDispatch()
 
   return(
     <div>
+
+    {user}의 장바구니
+
       <Table>
         <thead>
           <tr>
@@ -22,17 +30,21 @@ function Cart(){
           </tr>
         </thead>
         <tbody>
+        {/* redux store에서 가져온 데이터(=배열) 데이터바인딩하기 */}
+        {/* redux state 변경하기 */}
+
         {
-          stock.map(function(shoes, i){
-            return(
+          cart.map((shoes, i) => 
               <tr key={i}>
                 <td>1</td>
                 <td>{shoes.name}</td>
                 <td>{shoes.count}</td>
-                <td>수정중</td>
+                <td><button onClick={()=>{
+                // 변수 dispatch에 state변경함수 감싸주기 
+                  dispatch(changeName())
+                }}>+</button></td>
               </tr>
             )
-          })
         }
           
         </tbody>
--- shop/src/store.js
+++ shop/src/store.js
@@ -10,15 +10,31 @@ import { configureStore, createSlice } from '@reduxjs/toolkit'
 // // 여기는 state를 만드는 공간
 let user = createSlice({
   name : 'user',
-  initialState: 'kim'
+  initialState: 'kim',
+  // 리덕스 state 변경하는 법
+  // 1. state 수정해주는함수 만들고
+  // 2. 원할 때 그 함수 실행해달라고 store.js에 요청하기
+  // state 수정하는 함수 만들기
+  reducers : {
+    changeName(state){ //기존 state를 의미함. 
+      return 'john ' + state
+    }
+  }
 })
 
-let stock = createSlice({
-  name : 'stock',
+// 만든 수정함수 export 해야함
+export let { changeName } = user.actions
+
+
+
+let cart = createSlice({
+  name : 'cart',
   initialState: [
     {id : 0, name : 'White and Black', count : 2},
     {id : 2, name : 'Grey Yordan', count : 1}
-  ] 
+  ]
+  
+
 })
 
 
@@ -27,7 +43,7 @@ export default configureStore({
   // 여기는 state를 등록하는 공간
   reducer: {
     user : user.reducer,
-    stock : stock.reducer
+    cart : cart.reducer
   
   }
 })
