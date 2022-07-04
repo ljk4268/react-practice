c022bd844bed8096eb0e194141c8f6e504ace84d
ljk4268 <ljk4268@kakao.com>
2022-07-04 17:41:56 +0900

    create 복습

--- coding/src/App.js
+++ coding/src/App.js
@@ -4,32 +4,27 @@ import {useState} from 'react';
 
 
 function App() {
-  // useState는 배열을 반환하고 
-  // 0번쨰 데이터는 상태의 값이 들어가고 
-  // 1번째 데이터는 상태의 값을 변경할 때 사용하는 함수가 들어감 
+
   const [mode, setMode] = useState('WELCOME');
-  // 클릭한 타이틀마다 다른내용 보여주기 위해 id 변수하나 만들고 
+
   const [id, setId] = useState(null)
+  const [nextId, setNexId] = useState(4);
   
-  const topics = [
+  const [topics, setTopics] = useState([
     {id:1, title: 'html', body: ' html is...'},
     {id:2, title: 'css', body: ' css is...'},
     {id:3, title: 'javascript', body: ' javascript is...'}
-  ]
+  ])
 
+  // content //
   let content = null;
+
   if( mode === 'WELCOME' ) {
     content = <Article title="Welcome" body="Hello, World!"></Article>
   } else if ( mode === 'READ'){
-
-    // content = <Article title={title} body={body}></Article>에 동적으로 내용 넣어주기 위해 변수 title, body 생성해주고
     
     let title, body = null
 
-    // Nav 컴포넌트에서 전달받은 id
-    // props.onChangeMode(Number(e.target.id))로 전역상수변수 topics의 내용 찾기 위해 for문을 돌려줘 
-    // 전달받은 e.target.id랑 topics의 id랑 일치하면 
-    // 변수title에 해당 topics title 넣어주고 body 넣어주는거야 
     for( let i = 0; i < topics.length; i++ ){
       if( topics[i].id === id){
         title = topics[i].title;
@@ -37,9 +32,24 @@ function App() {
       }
     }
 
-    // 그 다음 동적으로 내용 보여주기 위해 변수들 중괄호로 가둬
     content = <Article title={title} body={body}></Article>
 
+  } else if ( mode === 'CREATE' ){
+    content = <Create onCreate={(_title, _body)=>{
+      // 전달받은 title, body를 topics에 넣기위해 객체로 만들어주는데
+      // id 필요하니까 변수 nextId 만들어서 id로 넣어줌 
+      const newTopic = {id:nextId, title:_title, body:_body}
+      // 깊은복사로 topics에 새로운 객체 넣어주고 
+      const newTopics = [...topics]
+      newTopics.push(newTopic)
+      setTopics(newTopics)
+      // read모드로 바꿔 바디내용 볼 수 있ㄱ ㅔ해주고 
+      setMode('READ')
+      // id도 바꿔주고 
+      setId(nextId)
+      // 다음에 추가될 내용을 위해 nextId+1 해줌 
+      setNexId(nextId+1)
+    }}></Create>
   }
 
   return (
@@ -54,21 +64,17 @@ function App() {
       <Nav 
       topics={topics}
       onChangeMode={(_id)=>{
-        // mode의 값이 변경되면 컴포넌트 함수가 새로 실행되면서
-        // 새로운 리턴값이 만들어지고 그 리턴값이 UI에 반영되게 하고픔! 
-        // 이땐 state를 사용해야함.
-        // useState라는 훅을 사용해야함. 
-        // useState에 들어가 있는 상태의 값을 변경할 때 사용하는 함수 사용하기
         setMode('READ')
-        // props.onChangeMode(Number(e.target.id))로 id 전달받으니까 전달받은 아이디 setId로 id 값 변경해줌 
         setId(_id)
       }}
       ></Nav>
 
-      
-      {/* 동적으로 변하니까 중괄호로 변수 content 넣어주고  */}
-
       {content}
+
+      <a href='/create' onClick={(e)=>{
+        e.preventDefault();
+        setMode('CREATE');
+      }}>Create</a>
       
     </div>
   );
@@ -94,11 +100,8 @@ function Nav(props){
     let t = props.topics[i]
     lis.push(
     <li key={t.id}>
-      {/* 태그의 속성으로 숫자를 넘기면 문자가 됨  */}
       <a id={t.id} href={'/read/'+t.id} onClick={(e)=>{
         e.preventDefault();
-        // 아래 있는 e.target.id는 문자.
-        // 숫자로 바꾸고 싶으면 Number함수 쓰기
         props.onChangeMode(Number(e.target.id));
       }}>{t.title}</a>
     </li>)
@@ -118,6 +121,28 @@ function Article(props){
     <article>
       <h2>{props.title}</h2>
       {props.body}
+    </article>
+  )
+}
+
+function Create(props){
+  return(
+    <article>
+      <h2>Create</h2>
+      {/* form태그는 submit을 했을 때 페이지가 리로드됨  */}
+      <form onSubmit={(e)=>{
+        e.preventDefault();
+        // name='title'인 태그 가져오는 방법(e.target.title)
+        // name='title'인 태그의 값을 가져오는 방법 
+        const title = e.target.title.value
+        const body = e.target.body.value
+        // props.onCreate(title, body)로 title, body 전달하고 
+        props.onCreate(title, body)
+      }}>
+        <p><input type='text' name='title' placeholder='title'/></p>
+        <p><textarea name='body' placeholder='body'></textarea></p>
+        <p><input type='submit' value='Create'/></p>
+      </form>
     </article>
   )
 }
